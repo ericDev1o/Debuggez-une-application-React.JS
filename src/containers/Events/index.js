68e4c200f1086253580ff811c70c2e07c4576cbc
Eric Faraut <eric.faraut@hotmail.fr>
2025-07-28 11:49:17 +0200

    Filter bug is fixed.

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,11 +13,13 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
+
+  const filteredEvents = 
+      !type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+      : data?.events.filter(event => event.type === type)
+    // eslint-disable-next-line spaced-comment
+    .filter((_/*event*/, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
@@ -31,7 +33,8 @@ const EventList = () => {
     setType(evtType);
   };
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+  const typeList = new Set(data?.events.map(event => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
@@ -42,7 +45,7 @@ const EventList = () => {
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={value => changeType(value)}
           />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
