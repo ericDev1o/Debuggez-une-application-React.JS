59f3cf1ee32b29b3dce3c944dbb6b2f1f2fb25c4
Eric Faraut <eric.faraut@hotmail.fr>
2025-07-31 11:46:59 +0200

    Menu test is done.

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ L'application est le site d'une agence evenementielle.
 
 ## Tests
 - `yarn test`
+
+### plan de non-regression generique (ne cassant pas au changement de donnees)
+1. les fonctionnalites critiques sont couvertes
+    a. le menu est cliquable
+    b. le defilement est accessible  
+    c. les tri & filtrage des evenements sont faits
+    d. les articles & sections statiques sont affichees
+    e. le formulaire est cliquable
+    f. le bas de page est affiche
+### cahier
+1. b.c.d.e.f.
+    i. 0 image
+    ii. image non-trouvee
+        
+    b. iii. pagination cliquable
--- src/containers/Menu/index.test.js
+++ src/containers/Menu/index.test.js
@@ -2,25 +2,50 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import Menu from "./index";
 
 describe("When Menu is created", () => {
-  it("a list of mandatories links and the logo are displayed", async () => {
+  it("must display a list of all links", async () => {
     render(<Menu />);
     await screen.findByText("Nos services");
     await screen.findByText("Nos réalisations");
     await screen.findByText("Notre équipe");
     await screen.findByText("Contact");
   });
-
-  describe("and a click is triggered on contact button", () => {
-    it("document location  href change", async () => {
-      render(<Menu />);
-      fireEvent(
-        await screen.findByText("Contact"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      expect(window.document.location.hash).toEqual("#contact");
-    });
-  });
 });
+
+describe(`& when 
+      user wants to get 
+      to any link target 
+      by clicking on menu link
+    then it must follow it 
+      until destination content is displayed`
+ , () => {
+  it("must get to services", async () => {
+    render(<Menu />);
+    fireEvent(
+      await screen.findByText("Nos services"),
+      new MouseEvent("click", {
+        cancelable: true,
+        bubbles: true,
+      })
+    );
+    expect(
+      screen.getByText("Nos services").
+      closest("a").
+      getAttribute("href")).
+    toEqual("#nos-services");
+  });
+  it("... must get to contact form", async () => {
+    render(<Menu />);
+    fireEvent(
+      await screen.findByText("Contact"),
+      new MouseEvent("click", {
+        cancelable: true,
+        bubbles: true,
+      })
+    );
+    expect(window.document.location.hash).toEqual("#contact");
+  });
+  it("should display Logo", async () => {
+    render(<Menu />);
+    expect(screen.findAllByAltText("Notre logo")).toBeTruthy();
+  });
+ });
