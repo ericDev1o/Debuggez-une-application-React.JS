f40c898f2eaeb6a01fcb434ccedfdc9a5b05639d
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-03 23:08:48 +0200

    EventCard is reviewed once.

--- src/components/EventCard/index.test.js
+++ src/components/EventCard/index.test.js
@@ -1,18 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import EventCard from "./index";
 
-describe("When a event card is created", () => {
-  it("an image is display with alt value", () => {
-    render(<EventCard imageSrc="http://src-image" imageAlt="Notre dernière prestation" date={new Date("2022-04-01")} 
-    title="test event"
-    
-    label="test label"
+describe("When EventCard is created", () => {
+  it("must display an image with alt value", async () => {
+    render(
+      <EventCard 
+        imageSrc="http://src-image" 
+        imageAlt="Notre dernière prestation" 
+        date={new Date("2022-04-01")} 
+        title="test event"
+        label="test label"
     />);
-    const imageElement = screen.getByTestId("card-image-testid");
+    const imageElement = await screen.getByTestId("card-image-testid");
+
     expect(imageElement).toBeInTheDocument();
     expect(imageElement.alt).toEqual("Notre dernière prestation");
   });
-  it("a title, a label and a month are displayed", () => {
+
+  it("must display a title & a label & a month", async () => {
     render(
       <EventCard
         imageSrc="http://src-image"
@@ -22,27 +27,30 @@ describe("When a event card is created", () => {
         date={new Date("2022-04-01")}
       />
     );
-    const titleElement = screen.getByText(/test event/);
-    const monthElement = screen.getByText(/avril/);
-    const labelElement = screen.getByText(/test label/);
+    const titleElement = await screen.getByText(/test event/);
+    const monthElement = await screen.getByText(/avril/);
+    const labelElement = await screen.getByText(/test label/);
+
     expect(titleElement).toBeInTheDocument();
     expect(labelElement).toBeInTheDocument();
     expect(monthElement).toBeInTheDocument();
   });
-  describe("with small props", () => {
-    it("a modifier small is added", () => {
-      render(
-        <EventCard
-          imageSrc="http://src-image"
-          imageAlt="Notre dernière prestation"
-          title="test event"
-          label="test label"
-          date={new Date("2022-04-01")}
-          small
-        />
-      );
-      const cardElement = screen.getByTestId("card-testid");
-      expect(cardElement.className.includes("EventCard--small")).toEqual(true);
-    });
+});
+
+describe("When EventCard has small prop", () => {
+  it("must postfix to className a modifier --small", async () => {
+    render(
+      <EventCard
+        imageSrc="http://src-image"
+        imageAlt="Notre dernière prestation"
+        title="test event"
+        label="test label"
+        date={new Date("2022-04-01")}
+        small="true"
+      />
+    );
+    const cardElement = await screen.getByTestId("card-testid");
+
+    expect(cardElement.className.includes("EventCard--small")).toBeTruthy();
   });
 });
