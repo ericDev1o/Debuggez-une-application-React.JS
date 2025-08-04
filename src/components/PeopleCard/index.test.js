6a14daa89bf771bd0be5c1b0abcb9c12e7fcbabc
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-04 09:52:18 +0200

    Service & People Cards are reviewed once.

--- src/components/PeopleCard/index.test.js
+++ src/components/PeopleCard/index.test.js
@@ -2,17 +2,21 @@ import { render, screen } from "@testing-library/react";
 import PeopleCard from "./index";
 
 describe("When a people card is created", () => {
-  it("an image is display with alt value", () => {
+  it("must display an image with alt value", async () => {
     render(
-      <PeopleCard imageSrc="http://src-image" imageAlt="image-alt-text" 
-      name="test name"
-      position="test position" />
+      <PeopleCard 
+        imageSrc="http://src-image" 
+        imageAlt="image-alt-text" 
+        name="test name"
+        position="test position" />
     );
-    const imageElement = screen.getByTestId("card-image-testid");
+    const imageElement = await screen.getByTestId("card-image-testid");
+
     expect(imageElement).toBeInTheDocument();
     expect(imageElement.alt).toEqual("image-alt-text");
   });
-  it("a title and a month are displayed", () => {
+
+  it("must display a title and a month", async () => {
     render(
       <PeopleCard
         imageSrc="http://src-image"
@@ -21,8 +25,9 @@ describe("When a people card is created", () => {
         position="test position"
       />
     );
-    const nameElement = screen.getByText(/test name/);
-    const titleElement = screen.getByText(/test position/);
+    const nameElement = await screen.getByText(/test name/);
+    const titleElement = await screen.getByText(/test position/);
+
     expect(nameElement).toBeInTheDocument();
     expect(titleElement).toBeInTheDocument();
   });
--- src/components/ServiceCard/index.test.js
+++ src/components/ServiceCard/index.test.js
@@ -2,21 +2,32 @@ import { render, screen } from "@testing-library/react";
 import ServiceCard from "./index";
 
 describe("When a service card is created", () => {
-  it("an image is display with alt value", () => {
+  it("must display an image with alt value", async () => {
     render(
-      <ServiceCard imageSrc="http://src-image" imageAlt="image-alt-text">{" "}</ServiceCard>
+      <ServiceCard 
+        imageSrc="http://src-image" 
+        imageAlt="image-alt-text"
+      >
+        {" "}
+      </ServiceCard>
     );
-    const imageElement = screen.getByTestId("card-image-testid");
+    const imageElement = await screen.getByTestId("card-image-testid");
+
     expect(imageElement).toBeInTheDocument();
     expect(imageElement.alt).toEqual("image-alt-text");
   });
-  it("a content is displayed", () => {
+
+  it("must display a content", async () => {
     render(
-      <ServiceCard imageSrc="http://src-image" imageAlt="image-alt-text">
+      <ServiceCard 
+        imageSrc="http://src-image" 
+        imageAlt="image-alt-text"
+      >
         This is the card content
       </ServiceCard>
     );
-    const contentElement = screen.getByText(/This is the card content/);
+    const contentElement = await screen.getByText(/This is the card content/);
+
     expect(contentElement).toBeInTheDocument();
   });
 });
