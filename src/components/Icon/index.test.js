7ba070eef2daaadeb2c5edff3baf13f40d66e6e4
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-04 17:27:05 +0200

    DataContext & Icon tests are viewed.

--- src/components/Icon/index.test.js
+++ src/components/Icon/index.test.js
@@ -3,15 +3,44 @@ import md5 from "md5";
 import Icon from ".";
 
 describe("Icon component", () => {
-    describe("When a icon is created with name twitch", () => {
-        it("the icon contain this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
+    describe("When an icon is created with name twitch", () => {
+        it("then it should contain this path hash value 327fbc38c8e878259c3ec35ef231517a", async () => {
             render(<Icon name="twitch" />)
-            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('327fbc38c8e878259c3ec35ef231517a')
+
+            await expect(md5(screen.getByTestId("twitch").getAttribute('d'))).toEqual('327fbc38c8e878259c3ec35ef231517a')
         });
     });
-    describe("When a icon is created with name facebook", () => {
-        it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
-            // to complete
+    describe("When an icon is created with name facebook", () => {
+        it("then it should contain this path hash value bbea4c9e40773b969fdb6e406059f853", async () => {
+            render(<Icon name="facebook" />)
+
+            await expect(md5(screen.getByTestId("facebook").getAttribute('d'))).toEqual("bbea4c9e40773b969fdb6e406059f853")
+        });
+    });
+    describe("When an icon is created with name twitter", () => {
+        it.skip("then it should contain this path hash value ...", async () => {
+            render(<Icon name="twitter" />)
+
+            await expect(md5(screen.getByTestId("X").getAttribute('d'))).toEqual(/* "to do" */)
+        });
+    });
+    describe("When an icon is created with name youtube", () => {
+        it.skip("then it should contain this path hash value ...", async () => {
+            render(<Icon name="youtube" />)
+
+            await expect(md5(screen.getByTestId("youtube1").getAttribute('d'))).toEqual(/* "to do" */)
+        });
+        it.skip("then it should contain this path hash value ...", async () => {
+            render(<Icon name="youtube" />)
+
+            await expect(md5(screen.getByTestId("youtube2").getAttribute('d'))).toEqual(/* "to do" */)
+        });
+    });
+    describe("When an icon is created with name close", () => {
+        it.skip("then it could contain this path hash value ...", async () => {
+            render(<Icon name="close" />)
+
+            await expect(md5(screen.getByTestId("close").getAttribute('d'))).toEqual(/* "to do" */)
         });
     });
 })
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -42,16 +42,16 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
+  it.skip("a list of events is displayed", () => {
     // to implement
   })
-  it("a list a people is displayed", () => {
+  it.skip("a list a people is displayed", () => {
     // to implement
   })
-  it("a footer is displayed", () => {
+  it.skip("a footer is displayed", () => {
     // to implement
   })
-  it("an event card, with the last event, is displayed", () => {
+  it.skip("an event card, with the last event, is displayed", () => {
     // to implement
   })
 });
