c467bdfd95a338d6186e3d2fe5316fb478674347
Eric Faraut <eric.faraut@hotmail.fr>
2025-08-08 12:09:47 +0200

    Service alt text is fixed.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -29,16 +29,20 @@ const Page = () => {
       </section>
       <section className="ServicesContainer">
         <h2 className="Title" id="nos-services">Nos services</h2>
-        <p>Nous organisons des événements sur mesure partout dans le monde</p>
+        <p>Nous organisons des évènements sur mesure partout dans le monde</p>
         <div className="ListContainer">
-          <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
+          <ServiceCard 
+            imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png"
+            imageAlt="dîner d'entreprise">
             <h3>Soirée d’entreprise</h3>
             Une soirée d’entreprise vous permet de réunir vos équipes pour un
             moment convivial afin de valoriser votre société en projetant une
             image dynamique. Nous vous proposons d’organiser pour vous vos
             diners et soirée d’entreprise
           </ServiceCard>
-          <ServiceCard imageSrc="/images/hall-expo.png">
+          <ServiceCard 
+            imageSrc="/images/hall-expo.png"
+            imageAlt="hall de conférences">
             <h3>Conférences</h3>
             724 events vous propose d’organiser votre évènement, quelle que soit
             sa taille, en s’adaptant à votre demande et à vos demandes. En tant
@@ -46,7 +50,9 @@ const Page = () => {
             parfait ainsi que des solutions inédites pour capter votre audience
             et faire de cet évènement un succès
           </ServiceCard>
-          <ServiceCard imageSrc="/images/sophia-sideri-LFXMtUuAKK8-unsplash1.png">
+          <ServiceCard 
+            imageSrc="/images/sophia-sideri-LFXMtUuAKK8-unsplash1.png"
+            imageAlt="expérience de réalité virtuelle avec casque">
             <h3>Experience digitale</h3>
             Notre agence experte en contenus immersifs offre des services de
             conseil aux entreprises, pour l’utilisation de la réalité virtuelle,
